f23958a8e9f7889aa21b527ddc00eb51f3f63331
Andrew Wanex <awanex2705@gmail.com>
2023-02-06 14:56:22 -0800

    chore: remove test console log

--- src/components/Navigation/NavLink/NavLink.tsx
+++ src/components/Navigation/NavLink/NavLink.tsx
@@ -9,13 +9,12 @@ import './NavLink.css'
 function NavLinkComponent(link: NavLink) {
   const { title, route, customClass = '' } = link
   const name: string = title.toLowerCase()
-  console.log(title, route, customClass, name)
+
   return (
     <div className={ `nav-link ${customClass}` }>
       <RouterNavLink
         key={ name }
         to={ route || `/${name}` }
-        
         aria-labelledby={ `nav-${name}` }
       >
         <span id={ `nav-${name}` }>{ title }</span>
