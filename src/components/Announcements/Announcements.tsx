efeac5b4bbc3488c1b4d9cb8cce8ea0ccdbede5f
Andrew Wanex <awanex2705@gmail.com>
2023-02-06 14:58:33 -0800

    feature: add initial announcements component

---
+++ src/components/Announcements/Announcements.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Announcements.css'
+
+
+function AnnouncementsComponent(): JSX.Element {
+  return (
+    <div className='announcements-container'>
+      TEST ANNOUNCEMENTS
+    </div>
+  )
+}
+
+
+export default AnnouncementsComponent
