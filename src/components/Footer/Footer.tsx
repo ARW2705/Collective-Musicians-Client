09dfcce0a1115d32d82388dd781ac574ec4ebaee
Andrew Wanex <awanex2705@gmail.com>
2023-02-06 10:34:36 -0800

    feature: add initial footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Footer.css'
+
+
+function Footer() {
+  return (
+    <footer>
+      Test Footer
+    </footer>
+  )
+}
+
+
+export default React.memo(Footer)
