1e4459fd5d3021a2a80186e063f9d4465d62d68d
Andrew Wanex <awanex2705@gmail.com>
2023-02-06 14:58:00 -0800

    feature: add initial image component

---
+++ src/components/Image/Image.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { ImageMetadata } from '../../models/image-metadata.interface'
+
+import './Image.css'
+
+
+function ImageComponent(imageData: ImageMetadata): JSX.Element {
+  const { src, alt, customClass = '' } = imageData
+
+  return (
+    <img
+      src={ `${process.env.PUBLIC_URL}/assets/images/${src}` }
+      alt={ alt }
+      className={ `image ${customClass}` }
+    />
+  )
+}
+
+
+export default React.memo(ImageComponent)
