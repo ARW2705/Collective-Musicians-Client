a56db007fcc140a74a38f7dbb3a82d164c008cac
Andrew Wanex <awanex2705@gmail.com>
2023-02-06 11:19:27 -0800

    feature: add home page to base route

--- src/routing/routes/AppRoutes.tsx
+++ src/routing/routes/AppRoutes.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import Home from '../../pages/Home/Home'
 
 function AppRoutes() {
   return (
     <Routes>
-      
+      <Route path='/' element={ <Home /> } />
     </Routes>
   )
 }
