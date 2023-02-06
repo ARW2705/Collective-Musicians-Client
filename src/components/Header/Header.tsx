28867682b22e299f657492b70cbc086a57753a62
Andrew Wanex <awanex2705@gmail.com>
2023-02-06 13:59:55 -0800

    feature: add navbar component to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 
+import { NavLink } from '../../models/interfaces'
+import NavbarComponent from '../Navigation/Navbar/Navbar'
+
 import './Header.css'
 
 
 function Header() {
+  const links: NavLink[] = [
+    { title: 'Organizations' },
+    { title: 'Venues'        },
+    { title: 'Philanthropy'  }
+  ]
+
   return (
     <header id='app-header'>
-      Test Header
+      <h1>Collective Musicians</h1>
+      <NavbarComponent links={ links } />
     </header>
   )
 }
