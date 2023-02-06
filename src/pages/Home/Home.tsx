dafe2565d63485934e527ba488406a7fb04e4ab0
Andrew Wanex <awanex2705@gmail.com>
2023-02-06 14:54:12 -0800

    feature: add main home page content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
+import Announcements from '../../components/Announcements/Announcements'
+import Image from '../../components/Image/Image'
+
 import './Home.css'
 
 
-function Home() {
+function Home(): JSX.Element {
+  const bannerMessage = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales. Velit ut tortor pretium viverra'
+  
   return (
-    <div id='home-page'>
-      Home Page
+    <div id='home-page' className='route'>
+      <div id='content-container'>
+        <Image
+          src='home_640.jpg'
+          alt='sheet music with orchestra in background'
+          customClass='banner-image'
+        />
+        <p>{ bannerMessage }</p>
+        <Announcements />
+      </div>
     </div>
   )
 }
