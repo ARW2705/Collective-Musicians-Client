b3b007db8a3e65dfada70d9f8bfceafaff184c32
Andrew Wanex <awanex2705@gmail.com>
2023-02-07 11:28:22 -0800

    feature: add initial query page and roadmap

---
+++ src/pages/Query/Query.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './Query.css'
+
+
+function QueryPage(): JSX.Element {
+  // TODO
+  // get spreadsheet title and sheet names
+  // get column names for each sheet
+  // user selects sheet name
+  // user selects columns to include (default to all)
+  // user adds a filter:
+  //   column name (dropdown) / condition (dropdown) / target (input)
+  // figure out how to form the condition groups
+  // submit the request
+  // append results to bottom of page
+
+  return <></>
+}
+
+
+export default QueryPage
