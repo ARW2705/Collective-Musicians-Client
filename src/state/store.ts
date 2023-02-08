430fc710e8de68bdb95af7fd00909bb0887dea44
Andrew Wanex <awanex2705@gmail.com>
2023-02-07 17:46:41 -0800

    feature: add redux store

---
+++ src/state/store.ts
@@ -0,0 +1,26 @@
+import { configureStore } from '@reduxjs/toolkit'
+import thunkMiddleware from 'redux-thunk'
+
+import spreadsheetMetadataReducer from './spreadsheet-metadata/slice'
+import { setSpreadsheetMetadata } from './spreadsheet-metadata/thunk'
+
+
+const store = configureStore({
+  reducer: {
+    spreadsheetMetadata: spreadsheetMetadataReducer
+  },
+  middleware: getDefaultMiddleware => (
+    getDefaultMiddleware().concat([
+      thunkMiddleware
+    ])
+  )
+})
+
+store.dispatch(setSpreadsheetMetadata())
+
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
+
+export default store
