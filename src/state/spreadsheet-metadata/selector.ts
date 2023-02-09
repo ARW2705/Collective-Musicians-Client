7a69ac222d713ed9172c930d77b9bd39f86db341
Andrew Wanex <awanex2705@gmail.com>
2023-02-08 17:06:33 -0800

    feature: add sheet selector to get sheet names

--- src/state/spreadsheet-metadata/selector.ts
+++ src/state/spreadsheet-metadata/selector.ts
@@ -2,6 +2,10 @@ import { SpreadsheetMetadata } from '../../models/interfaces'
 import { SheetMetadata } from '../../models/interfaces'
 
 
+function selectSheetNames(state: SpreadsheetMetadata): string[] {
+  return state.sheets.map((sheet: SheetMetadata) => sheet.name)
+}
+
 function selectSheet(state: SpreadsheetMetadata, sheetName: string): SheetMetadata | null {
   const sheet = state.sheets.find((sheet: SheetMetadata): boolean => sheet.name === sheetName)
   if (!sheet) return null
@@ -11,5 +15,6 @@ function selectSheet(state: SpreadsheetMetadata, sheetName: string): SheetMetada
 
 
 export {
+  selectSheetNames,
   selectSheet
 }
