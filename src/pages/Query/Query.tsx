151ff7600f1cf45c86295fba2ccd5d81b5a1e0a3
Andrew Wanex <awanex2705@gmail.com>
2023-02-13 09:25:41 -0800

    feature: add store listener for spreadsheet metadata refreshes

--- src/pages/Query/Query.tsx
+++ src/pages/Query/Query.tsx
@@ -1,17 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { SearchParams } from '../../models/interfaces'
+import { SpreadsheetMetadata, SearchParams } from '../../models/interfaces'
 import store from '../../state/store'
 import Query from '../../components/Query/Query'
-import Search from '../../components/Search/Search'
+// import Search from '../../components/Search/Search'
 
 import './Query.css'
 
 
 function QueryPage(): JSX.Element {
   const [searchParams, setSearchParams] = useState<SearchParams>()
-  const { spreadsheetMetadata } = store.getState()
+  const { spreadsheetMetadata: initialSpreadsheetMetadata } = store.getState()
+  const [ spreadsheetMetadata, setSpreadsheetMetadata ] = useState<SpreadsheetMetadata>(initialSpreadsheetMetadata)
 
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      setSpreadsheetMetadata(store.getState().spreadsheetMetadata)
+    })
+
+    return () => unsubscribe()
+  }, [])
+
+  //   <Search
+//   customClass='page-search-section'
+//   onSubmit={ (search: SearchParams) => setSearchParams(search) }
+// />
   return (
     <div id='query-page' className='route'>
       <h2>{ spreadsheetMetadata.name }</h2>
@@ -19,10 +32,7 @@ function QueryPage(): JSX.Element {
         customClass='page-query-section'
         searchParams={ searchParams }
       />
-      <Search
-        customClass='page-search-section'
-        onSubmit={ (search: SearchParams) => setSearchParams(search) }
-      />
+
     </div>
   )
 }
