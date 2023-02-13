6570d69b39a24951de3dfb17da7563f481275903
Andrew Wanex <awanex2705@gmail.com>
2023-02-13 09:23:30 -0800

    feature: change typing to single type that returns an array of type; add event handlers;

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,47 +1,70 @@
 import React, { MouseEvent, useEffect, useState } from 'react'
 
 import { SelectOption } from '../../models/interfaces'
+import Button from '../Button/Button'
 
 import './Select.css'
 
 
-export interface SelectAttributes<T, V> {
+export interface SelectProps<T> {
   title: string
   options: SelectOption<T>[]
-  onChange: (value: V) => void
+  onChange: (values: T[]) => void
   customClass?: string
   multi?: boolean
 }
 
-function SelectComponent<T, V = T>({ title, options, onChange, customClass, multi }: SelectAttributes<T, V>): JSX.Element {
+function SelectComponent<T>({ title, options, onChange: handleOnChange, customClass = '', multi }: SelectProps<T>): JSX.Element {
   const [ displayTitle, setDisplayTitle ] = useState(title)
   const [ showList, setShowList ] = useState(false)
-
-  const optionList = options.map((option: SelectOption<T>, index: number): JSX.Element => {
-    return (
-      <li key={ index } className='select-option'>
-        { option.label }
-      </li>
-    )
-  })
+  const [ selected, setSelected ] = useState<number[]>([])
+  const rowLimit: number = 8
+  const colCount: number = Math.ceil(options.length / rowLimit)
 
   useEffect(() => {
-    setDisplayTitle(title)
-  }, [title])
+    if (!selected.length || !options.length) return
 
-  const handleClick = (event: MouseEvent<HTMLDivElement>) => {
-    console.log(event)
+    if (multi) {
+      handleOnChange(selected.map((index: number): T => {
+        const { label, value }: SelectOption<T> = options[index]
+        return (value ?? label) as T
+      }))
+    } else {
+      const { label, value }: SelectOption<T> = options[selected[0]]
+      handleOnChange([(value ?? label) as T])
+      setDisplayTitle(label)
+    }
+  }, [selected, multi, options, handleOnChange])
+
+  const handleClick = (event: MouseEvent<HTMLUListElement>) => {
+    const targetIndex: number = parseInt((event.target as HTMLUListElement).getAttribute('data-index') || '')
+    if (!isNaN(targetIndex)) {
+      setSelected(multi ? [...selected, targetIndex] : [targetIndex])
+    }
   }
 
   return (
-    <div
-      className={ `app-select ${customClass}` }
-      onMouseLeave={ () => setShowList(false) }
-      onClick={ handleClick }
-    >
-      <h3>{ displayTitle }</h3>
+    <div className={ `app-select ${customClass}` }>
+      <Button
+        onClick={ () => setShowList(true) }
+        innerText={ displayTitle }
+        isFlat
+      />
       {
-        showList && <ul>{ optionList }</ul>
+        showList && 
+        <ul
+          style={ { gridTemplateColumns: `repeat(${colCount}, 1fr)` } }
+          onMouseLeave={ () => setShowList(false) }
+          onClick={ handleClick }
+        >
+          {
+            options.map((option: SelectOption<T>, index: number): JSX.Element => (
+              <li key={ index } data-index={ index } className={`select-option ${selected.includes(index) ? 'active' : ''}`}>
+                { option.label }
+              </li>
+            ))
+          }
+        </ul>
       }
     </div>
   )
