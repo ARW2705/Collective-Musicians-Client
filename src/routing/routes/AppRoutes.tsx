d9f82877e63634738dec9996a7d1eb5bb9b34283
Andrew Wanex <awanex2705@gmail.com>
2023-02-13 09:25:55 -0800

    feature: connect query page to router

--- src/routing/routes/AppRoutes.tsx
+++ src/routing/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import Home from '../../pages/Home/Home'
+import Query from '../../pages/Query/Query'
 
 import './AppRoutes.css'
 
@@ -9,6 +10,7 @@ function AppRoutes(): JSX.Element {
   return (
     <Routes>
       <Route path='/' element={ <Home /> } />
+      <Route path='/query' element={ <Query /> } />
     </Routes>
   )
 }
