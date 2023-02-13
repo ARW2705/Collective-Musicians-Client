f3f7e30d7049a3ac0a8ab436fa0841e03af0249a
Andrew Wanex <awanex2705@gmail.com>
2023-02-13 09:29:57 -0800

    feature: add isFlat prop to indicate non-raised buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,21 +3,22 @@ import React, { MouseEvent } from 'react'
 import './Button.css'
 
 
-export interface ButtonAttributes {
+export interface ButtonProps {
   onClick: (event: MouseEvent<HTMLButtonElement>) => void
   innerText: string
   innerElement?: JSX.Element
   ariaLabel?: string
   name?: string
   customClass?: string
+  isFlat?: boolean
   isDisabled?: boolean
 }
 
-function ButtonComponent({ onClick: handleOnClick, innerText, innerElement, ariaLabel, name, isDisabled, customClass }: ButtonAttributes): JSX.Element {  
+function ButtonComponent({ onClick: handleOnClick, innerText, innerElement, ariaLabel, name, isFlat, isDisabled, customClass }: ButtonProps): JSX.Element {  
   return (
     <button
       aria-label={ ariaLabel || innerText }
-      className={`app-button ${customClass}`}
+      className={`app-button ${isFlat ? 'flat-button' : ''} ${customClass}`}
       disabled={ isDisabled }
       name={ name }
       onClick={ handleOnClick }
