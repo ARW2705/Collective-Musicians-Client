beb1ba1e3d68572d4bba0a9ce7a941c89a44d91f
Andrew Wanex <awanex2705@gmail.com>
2023-02-13 09:36:00 -0800

    feature: change filter tracking to ref, change updateFilter function to match; conditionally render columns select and filter group to display after a sheet has been chosen; add component input interface

--- src/components/Query/Query.tsx
+++ src/components/Query/Query.tsx
@@ -1,71 +1,80 @@
-import React, { useEffect, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 
-import { SearchParams, SelectOption, QueryFilter, QueryCondition } from '../../models/interfaces'
+import { SearchParams, SelectOption, QueryFilter, QueryCondition, SpreadsheetMetadata } from '../../models/interfaces'
 import { selectSheetNames } from '../../state/spreadsheet-metadata/selector'
 import store from '../../state/store'
 import Button from '../Button/Button'
-import Collection from '../Collection/Collection'
+// import Collection from '../Collection/Collection'
 import FilterGroup from '../FilterGroup/FilterGroup'
 import Select from '../Select/Select'
-
+import { QueryContext } from './QueryContext'
+import Divider from '../Divider/Divider'
 import './Query.css'
 
 
-function QueryComponent(props : { customClass?: string, searchParams?: SearchParams }): JSX.Element {
+export interface QueryProps {
+  customClass?: string
+  searchParams?: SearchParams
+}
+
+function QueryComponent(props: QueryProps): JSX.Element {
   const { searchParams, customClass = '' } = props
   const { spreadsheetMetadata } = store.getState()
   const sheetNames: string[] = useSelector(selectSheetNames)
-  const [ columnOptions, setColumnOptions ] = useState<SelectOption[]>([])
-  const [ selectedSheetIndex, setSelectedSheetIndex ] = useState<number>(0)
-  const [ filter, setFilter ] = useState<QueryFilter>()
+  const [ selectedSheetIndex, setSelectedSheetIndex ] = useState<number>(-1)
   const [ queryResults, setQueryResults ] = useState([])
-
-  useEffect(() => {
-    const sheet = spreadsheetMetadata.sheets[selectedSheetIndex]
-    if (sheet) {
-      setColumnOptions(sheet.columnNames.map((name: string): SelectOption => ({ label: name })))
-    }
-  }, [selectedSheetIndex, spreadsheetMetadata.sheets])
+  const filter = useRef<QueryFilter>()
 
   const updateFilter = (kwargs: { includeColumns: string[] } | { conditions: QueryCondition[] }): void => {
-    setFilter((prevFilter: QueryFilter | undefined): QueryFilter | undefined => {
-      if (!kwargs) return prevFilter
-      if (!prevFilter && kwargs) return { filter: { ...kwargs } }
-      return {
-        filter: {
-          ...prevFilter,
-          ...kwargs
-        }
+    const prevFilter = filter.current
+    if (!kwargs) return
+    if (!prevFilter) {
+      filter.current = { filter: { ...kwargs } }
+    }
+    filter.current = {
+      filter: {
+        ...prevFilter,
+        ...kwargs
       }
-    })
+    }
+    console.log(filter.current)
   }
 
   const submitQuery = () => {
     console.log('submit query', selectedSheetIndex, filter)
   }
-
+//   <Collection
+//   customClass='page-query-results'
+//   items={ queryResults }
+// />
   return (
     <section className={ `query-container ${customClass}` }>
       <Select
         title='Select a Sheet'
         options={ sheetNames.map((name: string, index: number): SelectOption<number> => ({ label: name, value: index })) }
-        onChange={ (sheetIndex: number): void => setSelectedSheetIndex(sheetIndex) }
+        onChange={ (sheetIndex: number[]): void => setSelectedSheetIndex(sheetIndex[0]) }
       />
-      <Select
-        title='Include Columns'
-        options={ columnOptions }
-        onChange={ (includeColumns: string[]): void => updateFilter({ includeColumns }) }
-        multi
-      />
-      <FilterGroup
-        columns={ spreadsheetMetadata.sheets[selectedSheetIndex].columnNames }
-        onChange={ (conditions: QueryCondition[]): void => updateFilter({ conditions }) }
-      />
-      <Collection
-        customClass='page-query-results'
-        items={ queryResults }
-      />
+      <Divider />
+      {
+        selectedSheetIndex !== -1 &&
+        <>
+          <Select
+            title='Include Columns'
+            options={ spreadsheetMetadata.sheets[selectedSheetIndex].columnNames.map((name: string): SelectOption => ({ label: name })) }
+            onChange={ (includeColumns: string[]): void => updateFilter({ includeColumns }) }
+            multi
+          />
+          <Divider />
+          <QueryContext.Provider value={ { columnNames: spreadsheetMetadata.sheets[selectedSheetIndex].columnNames } }>
+            <FilterGroup
+              onChange={ (conditions: QueryCondition[]): void => updateFilter({ conditions }) }
+            />
+          </QueryContext.Provider>
+          <Divider />
+        </>
+      }
+
       <Button
         innerText='Submit'
         onClick={ submitQuery }
