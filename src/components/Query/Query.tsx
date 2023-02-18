4a5d5a8e446c3928e4aecdbde5e0927e706c956f
Andrew Wanex <awanex2705@gmail.com>
2023-02-17 18:10:41 -0800

    refactor: fix query filter construction on change

--- src/components/Query/Query.tsx
+++ src/components/Query/Query.tsx
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 
-import { SearchParams, SelectOption, QueryFilter, QueryCondition, SpreadsheetMetadata } from '../../models/interfaces'
+import { query } from '../../http/client'
+import { SearchParams, SelectOption, QueryResponse, QueryFilter, QueryParams, QueryCondition, SpreadsheetMetadata } from '../../models/interfaces'
 import { selectSheetNames } from '../../state/spreadsheet-metadata/selector'
 import store from '../../state/store'
 import Button from '../Button/Button'
-// import Collection from '../Collection/Collection'
+import QueryResultList from '../QueryResultList/QueryResultList'
 import FilterGroup from '../FilterGroup/FilterGroup'
 import Select from '../Select/Select'
 import { QueryContext } from './QueryContext'
@@ -18,36 +19,33 @@ export interface QueryProps {
   searchParams?: SearchParams
 }
 
-function QueryComponent(props: QueryProps): JSX.Element {
-  const { searchParams, customClass = '' } = props
+function QueryComponent({ searchParams, customClass = '' }: QueryProps): JSX.Element {
   const { spreadsheetMetadata } = store.getState()
   const sheetNames: string[] = useSelector(selectSheetNames)
   const [ selectedSheetIndex, setSelectedSheetIndex ] = useState<number>(-1)
-  const [ queryResults, setQueryResults ] = useState([])
-  const filter = useRef<QueryFilter>()
+  const [ queryResults, setQueryResults ] = useState<QueryResponse[]>([])
+  const includeColumns = useRef<string[]>([])
+  const filterConditions = useRef<QueryCondition[]>([])
 
-  const updateFilter = (kwargs: { includeColumns: string[] } | { conditions: QueryCondition[] }): void => {
-    const prevFilter = filter.current
-    if (!kwargs) return
-    if (!prevFilter) {
-      filter.current = { filter: { ...kwargs } }
-    }
-    filter.current = {
-      filter: {
-        ...prevFilter,
-        ...kwargs
+  const submitQuery = async () => {
+    console.log('submit query', selectedSheetIndex, includeColumns.current, filterConditions.current)
+    try {
+      let queryParams: QueryParams = { sheetName: sheetNames[selectedSheetIndex], page: 1, limit: 5 }
+      let queryFilter: { includeColumns?: string[], conditions?: QueryCondition[] } = {}
+      if (includeColumns.current.length) {
+        Object.assign(queryFilter, { includeColumns: includeColumns.current })
       }
+      if (filterConditions.current.length) {
+        Object.assign(queryFilter, { conditions: filterConditions.current })
+      }
+
+      const response = await query<QueryResponse[]>('spreadsheets/sheet/query', queryParams, { filter: queryFilter })
+      setQueryResults(response)
+    } catch (error) {
+      console.log('got error trying to submit query', error)
     }
-    console.log(filter.current)
   }
 
-  const submitQuery = () => {
-    console.log('submit query', selectedSheetIndex, filter)
-  }
-//   <Collection
-//   customClass='page-query-results'
-//   items={ queryResults }
-// />
   return (
     <section className={ `query-container ${customClass}` }>
       <Select
@@ -62,23 +60,30 @@ function QueryComponent(props: QueryProps): JSX.Element {
           <Select
             title='Include Columns'
             options={ spreadsheetMetadata.sheets[selectedSheetIndex].columnNames.map((name: string): SelectOption => ({ label: name })) }
-            onChange={ (includeColumns: string[]): void => updateFilter({ includeColumns }) }
+            onChange={ (columns: string[]): void => { includeColumns.current = columns } }
             multi
           />
           <Divider />
           <QueryContext.Provider value={ { columnNames: spreadsheetMetadata.sheets[selectedSheetIndex].columnNames } }>
             <FilterGroup
-              onChange={ (conditions: QueryCondition[]): void => updateFilter({ conditions }) }
+              onChange={ (conditions: QueryCondition[]): void => { filterConditions.current = conditions } }
             />
           </QueryContext.Provider>
           <Divider />
         </>
       }
-
       <Button
+        name='submit-query'
         innerText='Submit'
         onClick={ submitQuery }
       />
+      {
+        !!queryResults.length &&
+        <QueryResultList
+          customClass='page-query-results'
+          queryResults={ queryResults }
+        />
+      }
     </section>
   )
 }
