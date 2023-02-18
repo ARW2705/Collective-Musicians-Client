ddfba68ff2a09f9fc5b0795d72ab98c08634bd72
Andrew Wanex <awanex2705@gmail.com>
2023-02-17 18:07:59 -0800

    feature: add function to run validators for a given value

---
+++ src/shared/validation/validate.ts
@@ -0,0 +1,14 @@
+import { ValidatorFn, ValidationError } from '../../models/interfaces'
+
+export function validate<T>(value: T | null, validators: ValidatorFn<T>[]): ValidationError<T> {
+  if (!validators) return {}
+
+  return validators.reduce(
+    (errors: ValidationError<T>, validator: ValidatorFn<T>): ValidationError<T> => {
+      const error: ValidationError<T> | null = validator(value)
+      if (error) Object.assign(errors, error)
+      return errors
+    },
+    {}
+  )
+}
