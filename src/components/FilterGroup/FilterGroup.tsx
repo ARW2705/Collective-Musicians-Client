fdabb474db91482068d355b40e8be73b78522e0c
Andrew Wanex <awanex2705@gmail.com>
2023-02-17 17:54:16 -0800

    feature: add filter group component

---
+++ src/components/FilterGroup/FilterGroup.tsx
@@ -0,0 +1,65 @@
+import React, { useRef, useState } from 'react'
+
+import { QueryCondition } from '../../models/interfaces'
+
+import Button from '../Button/Button'
+import Filter from '../Filter/Filter'
+
+import './FilterGroup.css'
+
+
+export interface FilterGroupProps {
+  onChange: (conditions: QueryCondition[]) => void
+}
+
+function FilterGroupComponent({ onChange }: FilterGroupProps): JSX.Element {
+  const [ filterComponents, setFilterComponents ] = useState<JSX.Element[]>([])
+  const groups = useRef<{[key: number]: QueryCondition}>({})
+  const groupKeys = useRef<number>(0)
+
+  const onSubmit = (conditions: QueryCondition, groupKey: number) => {
+    groups.current = {
+      ...groups.current,
+      [groupKey]: {
+        ...groups.current[groupKey],
+        ...conditions
+      }
+    }
+
+    let filterGroups: QueryCondition[] = []
+    for (const key in groups.current) {
+      filterGroups = [...filterGroups, groups.current[key]]
+    }
+    onChange(filterGroups)
+  }
+    
+  const addFilter = (): void => {
+    setFilterComponents((prevFilters: JSX.Element[]) => {
+      const groupKey = groupKeys.current
+      groupKeys.current = groupKey + 1
+      if (prevFilters.length > 0) {
+        prevFilters = [...prevFilters, <div className='group-separator' key={ `separator-${prevFilters.length}`}><span>OR</span></div>]
+      }
+      prevFilters = [
+        ...prevFilters,
+        <Filter onSubmit={ onSubmit } groupKey={ groupKey } key={ prevFilters.length + 1 } />
+      ]
+      return prevFilters
+    })
+  }
+
+  return (
+    <section className={`filter-group-container ${filterComponents.length > 0 ? 'has-filters' : ''}`}>
+      <Button
+        name='add-filter-group'
+        customClass='add-filter-group-button'
+        innerText='Add Filter Group'
+        onClick={ addFilter }
+      />
+      { filterComponents }
+    </section>
+  )
+}
+
+
+export default FilterGroupComponent
