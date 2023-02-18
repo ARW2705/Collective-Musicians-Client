a790b9fef95d22ad514b1635d488a002b6ce49da
Andrew Wanex <awanex2705@gmail.com>
2023-02-17 18:01:26 -0800

    feature: add list view component

---
+++ src/components/ListView/ListView.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import { Primitive } from '../../models/interfaces'
+
+import './ListView.css'
+
+
+function buildListElement(list: Primitive[]): JSX.Element[] {
+  return list.reduce((itemElements: JSX.Element[], listItem: Primitive, index: number): JSX.Element[] => {
+    const itemElement: JSX.Element = (
+      <li className='list-view-item' key={ index }>
+        { listItem }
+      </li>
+    )
+
+    return [...itemElements, itemElement]
+  }, [])
+}
+
+export interface ListViewProps {
+  list: Primitive[]
+  customClass?: string
+}
+
+function ListViewComponent({ list, customClass = '' }: ListViewProps): JSX.Element {
+  if (list.length === 0) return <></>
+
+  const listElement: JSX.Element[] = buildListElement(list)
+  
+  return (
+    <ul className={ `list-view-container ${customClass}` }>
+      { listElement }
+    </ul>
+  )
+}
+
+
+export default React.memo(ListViewComponent)
