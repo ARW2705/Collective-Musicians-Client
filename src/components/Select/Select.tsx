b07bd352aa10fb23bc10a0478c817c5c967210f9
Andrew Wanex <awanex2705@gmail.com>
2023-02-17 18:11:47 -0800

    feature: add validation error catching to select component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,6 +1,8 @@
-import React, { MouseEvent, useEffect, useState } from 'react'
+import React, { MouseEvent, useEffect, useRef, useState } from 'react'
 
-import { SelectOption } from '../../models/interfaces'
+import { SelectOption, ValidatorFn, ValidationError } from '../../models/interfaces'
+import { validate } from '../../shared/validation/validate'
+
 import Button from '../Button/Button'
 
 import './Select.css'
@@ -9,50 +11,75 @@ import './Select.css'
 export interface SelectProps<T> {
   title: string
   options: SelectOption<T>[]
-  onChange: (values: T[]) => void
+  onChange: (values: T[], errors: ValidationError<T>) => void
+  validators?: ValidatorFn<T>[]
   customClass?: string
   multi?: boolean
+  rowLimit?: number
+  openDirection?: 'up' | 'center' | 'down'
+  reset?: boolean
 }
 
-function SelectComponent<T>({ title, options, onChange: handleOnChange, customClass = '', multi }: SelectProps<T>): JSX.Element {
+function SelectComponent<T>({ title, options, onChange: handleOnChange, validators = [], customClass = '', multi, rowLimit = 8, openDirection = 'down', reset = false }: SelectProps<T>): JSX.Element {
   const [ displayTitle, setDisplayTitle ] = useState(title)
   const [ showList, setShowList ] = useState(false)
   const [ selected, setSelected ] = useState<number[]>([])
-  const rowLimit: number = 8
+  const [ errorState, setErrorState ] = useState<{ errors: ValidationError<T>, show: boolean }>({
+    errors: {},
+    show: false
+  })
+  const onInit = useRef<boolean>(true)
   const colCount: number = Math.ceil(options.length / rowLimit)
 
   useEffect(() => {
-    if (!selected.length || !options.length) return
+    if (onInit.current) {
+      onInit.current = false
+      return
+    }
+    setSelected([])
+    setErrorState({ errors: {}, show: false })
+  }, [reset])
+
+  useEffect(() => {
+    if (!selected.length || !options.length) {
+      setDisplayTitle(title)
+      return
+    }
 
     if (multi) {
       handleOnChange(selected.map((index: number): T => {
         const { label, value }: SelectOption<T> = options[index]
         return (value ?? label) as T
-      }))
+      }), errorState.errors)
     } else {
       const { label, value }: SelectOption<T> = options[selected[0]]
-      handleOnChange([(value ?? label) as T])
+      handleOnChange([(value ?? label) as T], errorState.errors)
       setDisplayTitle(label)
     }
-  }, [selected, multi, options, handleOnChange])
+  }, [title, selected, multi, options, handleOnChange, errorState])
 
   const handleClick = (event: MouseEvent<HTMLUListElement>) => {
     const targetIndex: number = parseInt((event.target as HTMLUListElement).getAttribute('data-index') || '')
     if (!isNaN(targetIndex)) {
+      setErrorState({ errors: {}, show: false })
       setSelected(multi ? [...selected, targetIndex] : [targetIndex])
+    } else {
+      setErrorState({ errors: validate<T>(null, validators), show: false })
     }
   }
 
   return (
-    <div className={ `app-select ${customClass}` }>
+    <div className={ `app-select ${openDirection !== 'center' ? 'set-position' : ''} ${customClass}` }>
       <Button
+        name='open-select'
         onClick={ () => setShowList(true) }
         innerText={ displayTitle }
-        isFlat
+        flat
       />
       {
         showList && 
         <ul
+          className={ `open-${openDirection}` }
           style={ { gridTemplateColumns: `repeat(${colCount}, 1fr)` } }
           onMouseLeave={ () => setShowList(false) }
           onClick={ handleClick }
