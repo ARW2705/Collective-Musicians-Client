6b4ce4997b918d90fd3d0d03451066c02f47055e
Andrew Wanex <awanex2705@gmail.com>
2023-02-21 16:59:26 -0800

    feature: add pagination context

--- src/pages/Query/Query.tsx
+++ src/pages/Query/Query.tsx
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from 'react'
 
-import { SpreadsheetMetadata, SearchParams } from '../../models/interfaces'
+import { SpreadsheetMetadata } from '../../models/spreadsheet-metadata'
+import { SearchParams        } from '../../models/search-params'
+
 import store from '../../state/store'
+
+import { PaginationContext } from '../../contexts/Pagination/PaginationContext'
+
 import Query from '../../components/Query/Query'
+
 // import Search from '../../components/Search/Search'
 
 import './Query.css'
 
 
 function QueryPage(): JSX.Element {
+  const [ page, setPage ] = useState<number>(1)
+  const [ pageLimit, setPageLimit ] = useState<number>(5)
   const [searchParams, setSearchParams] = useState<SearchParams>()
   const { spreadsheetMetadata: initialSpreadsheetMetadata } = store.getState()
   const [ spreadsheetMetadata, setSpreadsheetMetadata ] = useState<SpreadsheetMetadata>(initialSpreadsheetMetadata)
@@ -28,11 +36,12 @@ function QueryPage(): JSX.Element {
   return (
     <div id='query-page' className='route'>
       <h2>{ spreadsheetMetadata.name }</h2>
-      <Query
-        customClass='page-query-section'
-        searchParams={ searchParams }
-      />
-
+      <PaginationContext.Provider value={ { page, setPage, pageLimit, setPageLimit } }>
+        <Query
+          customClass='page-query-section'
+          searchParams={ searchParams }
+        />
+      </PaginationContext.Provider>
     </div>
   )
 }
