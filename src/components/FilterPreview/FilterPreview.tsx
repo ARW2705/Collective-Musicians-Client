6f3b29a8bf4bc6aa01fa92b0a6c9a04d05cb5d99
Andrew Wanex <awanex2705@gmail.com>
2023-02-28 17:26:42 -0800

    refactor: update filter preview component to conform with new filter models; add remove button

--- src/components/FilterPreview/FilterPreview.tsx
+++ src/components/FilterPreview/FilterPreview.tsx
@@ -1,48 +1,67 @@
 import React, { memo } from 'react'
+import { FaMinusCircle } from 'react-icons/fa'
 
 import { QueryCondition } from '../../models/query-condition'
 import { QueryTarget    } from '../../models/query-target'
+import { SingleQueryArgs } from '../../models/single-query-args'
 import { SelectOption   } from '../../models/select-option'
 
 import { FILTER_CONDITION_OPTIONS } from '../../shared/filter-condition-defs'
 
+import Button from '../Button/Button'
+
 import './FilterPreview.css'
 
 
-function buildPreviewElement(key: string, index: number, condition: string, target: QueryTarget): JSX.Element {
+function buildPreviewElement(column: string, index: number, selectOption: SelectOption, target: QueryTarget, handleOnClick: (query: SingleQueryArgs) => void): JSX.Element {
   return (
-    <div className='filter-preview' key={ `${key}${index}` }>
-      <span>{ key }</span>
-      <span>{ condition }</span>
-      <span>{ target }</span>
+    <div className='filter-preview' key={ `${column}${index}` }>
+      <div className='filter-text'>
+        <span>{ column }</span>
+        <span>{ selectOption.label }</span>
+        <span>{ target }</span>
+      </div>
+      <Button
+        name='remove-filter'
+        customClass='remove-button'
+        innerElement={ <FaMinusCircle /> }
+        onClick={ () => handleOnClick({ column, target, condition: selectOption?.value || selectOption.label }) }
+        flat
+      />
     </div>
   )
 }
 
 export interface FilterPreviewProps {
   filters: QueryCondition | undefined
+  onClick: (query: SingleQueryArgs) => void
 }
 
-function FilterPreviewComponent({ filters }: FilterPreviewProps): JSX.Element {
+function FilterPreviewComponent({ filters, onClick: handleOnClick }: FilterPreviewProps): JSX.Element {
   let previews: JSX.Element[] = []
-  let index: number = 0
+  let flattenedIndex: number = 0
+  
   for (const key in filters) {
-    const selectOption: SelectOption | undefined = FILTER_CONDITION_OPTIONS
-      .find((option: SelectOption): boolean => option.value === filters[key].condition)
-    const condition: string = selectOption ? selectOption.label : filters[key].condition
-    const preview: JSX.Element = buildPreviewElement(key, index, condition, filters[key].target)
+    for (let i = 0; i < filters[key].length; i++) {
+      const queryArgs = filters[key][i]
+      const selectOption: SelectOption | undefined = FILTER_CONDITION_OPTIONS
+        .find((option: SelectOption): boolean => option.value === queryArgs.condition)
+      if (!selectOption) {
+        throw new Error(`Select option of ${key} is not valid`)
+      }
 
-    if (index % 2 !== 0) {
-      previews = [
-        ...previews,
-        <div className='filter-separator' key={ index }>
-          <span>AND</span>
-        </div>
-      ]
+      if (flattenedIndex > 0) {
+        previews = [
+          ...previews,
+          <div className='filter-separator' key={ flattenedIndex }>
+            <span>AND</span>
+          </div>
+        ]
+      }
+
+      previews = [...previews, buildPreviewElement(key, flattenedIndex, selectOption, queryArgs.target, handleOnClick)]
+      flattenedIndex++
     }
-
-    previews = [...previews, preview]
-    index++
   }
 
   return (
