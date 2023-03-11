d3180837954f862fcb0980cf5cf98f514eebd510
Andrew Wanex <awanex2705@gmail.com>
2023-03-10 16:02:04 -0800

    refactor: remove default export, add named export

--- src/shared/shallow-compare.ts
+++ src/shared/shallow-compare.ts
@@ -1,5 +1,5 @@
-function shallowCompare<T>(arg1: T, arg2: T): boolean {
+function compare<T>(arg1: T, arg2: T): boolean {
   return JSON.stringify(arg1) === JSON.stringify(arg2)
 }
 
-export default shallowCompare
+export { compare }
