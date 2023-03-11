a1e60d507175ebbafd37e7d5c50da7f21b008ee7
Andrew Wanex <awanex2705@gmail.com>
2023-03-10 16:01:17 -0800

    refactor: extract code to build new selected array to its own function

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,20 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 import { SelectContext   } from '../../contexts/Select/SelectContext'
+import { ErrorState      } from '../../models/error-state'
 import { SelectOption    } from '../../models/select-option'
 import { ValidatorFn     } from '../../models/validator-function'
 import { ValidationError } from '../../models/validation-error'
+import { remove          } from '../../shared/remove-at'
+import { compare         } from '../../shared/shallow-compare'
 import { validate        } from '../../shared/validation/validate'
-import compare             from '../../shared/shallow-compare'
 
 import Button        from '../Button/Button'
-import SelectPreview from '../SelectPreview/SelectPreview'
 import SelectOptions from '../SelectOptions/SelectOptions'
+import SelectPreview from '../SelectPreview/SelectPreview'
 
 import './Select.css'
 
 
-function getMultiSelections<T>(selected: number[], selectAllFlag: number, options: SelectOption<T>[]) {
+/**
+ * Generate an array of values to be accepted as the user's selected values
+ * 
+ * @param selected - array of indices selected by the user
+ * @param selectAllFlag - index that indicates all values should be selected
+ * @param options - array of select options
+ * @return array of normalized user selected values
+ */
+function getMultiSelections<T>(selected: number[], selectAllFlag: number, options: SelectOption<T>[]): T[] {
   if (selected.includes(selectAllFlag)) {
     return options.map(({ label, value }: SelectOption<T>): T => ((value ?? label) as T))
   }
@@ -24,6 +34,22 @@ function getMultiSelections<T>(selected: number[], selectAllFlag: number, option
     return (value ?? label) as T
   })
 }
+
+/**
+ * Generate an array of selected option indices
+ * 
+ * @param selected - the currently selected indices
+ * @param targetIndex - the index to change; if the index already exists, remove it, otherwise add to the end of the array
+ * @return array of indices
+ */
+function getMultiSelectIndices(selected: number[], targetIndex: number): number[] {
+  const currentSelectedIndex: number = selected.findIndex(selectedIndex => (
+    selectedIndex === targetIndex
+  ))
+  if (currentSelectedIndex === -1) return [...selected, targetIndex]
+  return remove(selected, currentSelectedIndex)
+}
+
 
 export interface SelectProps<T> {
   onChange: (values: T[], errors: ValidationError<T>) => void
@@ -37,11 +63,23 @@ export interface SelectProps<T> {
   grid?: boolean
 }
 
-function SelectComponent<T>({ customClass = '', validators = [], reset = false, grid = false, multi = false, defaultSelections = [], onChange: handleOnChange, options, title }: SelectProps<T>): JSX.Element {
+function SelectComponent<T>(props: SelectProps<T>): JSX.Element {
+  const {
+    options,
+    title,
+    customClass = '',
+    validators = [],
+    reset = false,
+    grid = false,
+    multi = false,
+    defaultSelections = [],
+    onChange: handleOnChange
+  } = props
+
   const [ displayTitle, setDisplayTitle ] = useState(title)
   const [ showList, setShowList ] = useState(false)
   const [ selected, setSelected ] = useState<number[]>(defaultSelections)
-  const [ errorState, setErrorState ] = useState<{ errors: ValidationError<T>, show: boolean }>({ errors: {}, show: false })
+  const [ errorState, setErrorState ] = useState<ErrorState<T>>({ errors: {}, show: false })
   const previousSelected = useRef<number[]>([])
   const previousOptions = useRef<SelectOption<T>[]>()
   const resetLatch = useRef<boolean>(reset)
@@ -71,11 +109,20 @@ function SelectComponent<T>({ customClass = '', validators = [], reset = false, 
     } else if (selected.length > 0) {
       const { label, value }: SelectOption<T> = options[selected[0]]
       handleOnChange([(value ?? label) as T], errorState.errors)
-      setDisplayTitle(label)
-    } else {
-      setDisplayTitle(title)
     }
-  }, [selected, multi, options, handleOnChange, errorState, title, selectAllFlag])
+  }, [selected, multi, options, handleOnChange, errorState, selectAllFlag])
+
+  useEffect(() => {
+    if (multi) return
+
+    let newTitle: string = title
+    if (selected.length > 0) {
+      const { label }: SelectOption<T> = options[selected[0]]
+      newTitle = label
+    }
+
+    setDisplayTitle(newTitle)
+  }, [multi, selected, options, title])
 
   const handleClick = (targetIndex: number): void => {
     if (isNaN(targetIndex)) return
@@ -83,16 +130,11 @@ function SelectComponent<T>({ customClass = '', validators = [], reset = false, 
     let errors: ValidationError<T>
     let selections: number[] = selected
     if (multi) {
-      const currentSelectedIndex: number = selected.findIndex(selectedIndex => selectedIndex === targetIndex)
-      if (currentSelectedIndex === -1) {
-        selections = [...selections, targetIndex]
-      } else {
-        selections = [...selected.slice(0, currentSelectedIndex), ...selected.slice(currentSelectedIndex + 1, selected.length)]
-      }
+      selections = getMultiSelectIndices(selected, targetIndex)
       errors = validate<T>(selections as T, validators)
     } else {
-      errors = validate<T>([targetIndex] as T, validators)
       selections = [targetIndex]
+      errors = validate<T>([targetIndex] as T, validators)
     }
 
     setSelected(selections)
