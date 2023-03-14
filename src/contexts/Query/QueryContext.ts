e14f8cb41d5b901368bdf71decef57bcda62cda3
Andrew Wanex <awanex2705@gmail.com>
2023-03-14 13:26:50 -0700

    refactor: update context to use the query component's reducer state

--- src/contexts/Query/QueryContext.ts
+++ src/contexts/Query/QueryContext.ts
@@ -1,15 +1,21 @@
-import { createContext } from 'react'
+import { createContext, Dispatch, MutableRefObject } from 'react'
 
-import { QueryResponse } from '../../models/query-response'
+import { QueryCondition } from '../../models/query-condition'
+import { ReducerAction } from '../../models/reducer-action'
+import { QueryPayload, QueryState, QueryAction, initialState } from '../../components/Query/query-reducer'
 
 export interface QueryContextProps {
-  columnNames: string[]
-  queryInProgress: boolean
-  queryResponse: QueryResponse | undefined
+  sheetNames: string[]
+  filterConditions: MutableRefObject<QueryCondition[]>
+  submitQuery: (submit?: boolean) => Promise<void>
+  state: QueryState
+  dispatch: Dispatch<ReducerAction<QueryPayload, QueryAction>>
 }
 
 export const QueryContext = createContext<QueryContextProps>({
-  columnNames: [],
-  queryInProgress: false,
-  queryResponse: undefined
+  sheetNames: [],
+  filterConditions: { current: [] },
+  submitQuery: () => Promise.resolve(),
+  state: initialState,
+  dispatch: () => {}
 })
