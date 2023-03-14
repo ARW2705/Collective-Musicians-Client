c1271735204fed966ed6db28859c9e6553379819
Andrew Wanex <awanex2705@gmail.com>
2023-03-14 13:39:00 -0700

    refactor: use new query creator component

--- src/components/Query/Query.tsx
+++ src/components/Query/Query.tsx
@@ -4,25 +4,21 @@ import { useSelector } from 'react-redux'
 import { QueryCondition } from '../../models/query-condition'
 import { QueryParams    } from '../../models/query-params'
 import { QueryResponse  } from '../../models/query-response'
-import { SelectOption   } from '../../models/select-option'
 import { SearchParams   } from '../../models/search-params'
 
 import { query } from '../../http/client'
 
 import { selectSheetNames } from '../../state/spreadsheet-metadata/selector'
-import store                from '../../state/store'
 
 import { PaginationContext } from '../../contexts/Pagination/PaginationContext'
 import { QueryContext      } from '../../contexts/Query/QueryContext'
 
-import Button          from '../Button/Button'
-import Divider         from '../Divider/Divider'
-import Filter          from '../Filter/Filter'
 import Loader          from '../Loaders/Loader'
+import QueryCreator    from '../QueryCreator/QueryCreator'
 import QueryResultList from '../QueryResultList/QueryResultList'
-import Select          from '../Select/Select'
 
-import { reducer, initialState, QueryAction } from './query-reducer'
+import reducer, { initialState, QueryAction } from './query-reducer'
+
 import './Query.css'
 
 
@@ -32,7 +28,6 @@ export interface QueryProps {
 }
 
 function QueryComponent({ customClass = '', searchParams }: QueryProps): JSX.Element {
-  const { spreadsheetMetadata } = store.getState()
   const sheetNames: string[] = useSelector(selectSheetNames)
   const { page, pageLimit } = useContext(PaginationContext)
   const [ state, dispatch ] = useReducer(reducer, initialState)
@@ -77,51 +72,14 @@ function QueryComponent({ customClass = '', searchParams }: QueryProps): JSX.Ele
 
   return (
     <section className={ `query-container ${customClass}` }>
-      {
-        sheetNames.length > 0 &&
-        <Select
-          title='Select a Sheet'
-          options={ sheetNames.map((name: string, index: number): SelectOption<number> => ({ label: name, value: index })) }
-          onChange={ (sheetIndex: number[]): void => dispatch({ type: QueryAction.SET_SHEET_INDEX, payload: sheetIndex[0] }) }
-        />
-      }
-      <QueryContext.Provider value={ { columnNames: state.selectedSheetIndex === -1 ? [] : spreadsheetMetadata.sheets[state.selectedSheetIndex].columnNames, queryInProgress: state.queryInProgress, queryResponse: state.queryResponse } }>
-        {
-          state.selectedSheetIndex !== -1 &&
-          <>
-            <Divider />
-            <Select
-              title='Include Columns'
-              options={ spreadsheetMetadata.sheets[state.selectedSheetIndex].columnNames.map((name: string): SelectOption => ({ label: name })) }
-              onChange={ (columns: string[]): void => dispatch({ type: QueryAction.SET_INCLUDE_COLUMNS, payload: columns }) }
-              defaultSelections={ [spreadsheetMetadata.sheets[state.selectedSheetIndex].columnNames.length] }
-              reset={ state.reset }
-              grid
-              multi
-            />
-            <Divider />
-            <Filter
-              onChange={ (conditions: QueryCondition[]): void => { filterConditions.current = conditions } }
-              reset={ state.reset }
-            />
-            <Divider />
-            <Button
-              name='submit-query'
-              onClick={ () => submitQuery(true) }
-              disabled={ state.queryInProgress }
-            >
-              Submit Query
-            </Button>
-            { !!state.queryResponse?.results.length &&
-              <Button
-                name='clear-query'
-                onClick={ () => dispatch({ type: QueryAction.SET_QUERY_RESPONSE, payload: undefined }) }
-              >
-                Clear Query
-              </Button>
-            }
-          </>
-        }
+      <QueryContext.Provider value={ {
+        sheetNames,
+        filterConditions,
+        submitQuery,
+        state,
+        dispatch
+      } }>
+        <QueryCreator />
         <Loader
           show={ state.queryInProgress }
           type='bar'
