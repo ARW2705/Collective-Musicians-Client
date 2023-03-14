8074747a98d5c6cf79d3538572eb10f2f743209a
Andrew Wanex <awanex2705@gmail.com>
2023-03-14 13:37:09 -0700

    feature: extract query inputs to its own component

---
+++ src/components/QueryCreator/QueryCreator.tsx
@@ -0,0 +1,74 @@
+import React, { memo, useContext } from 'react'
+import { useSelector } from 'react-redux'
+
+import { QueryContext      } from '../../contexts/Query/QueryContext'
+import { QueryCondition    } from '../../models/query-condition'
+import { SelectOption      } from '../../models/select-option'
+import { selectColumnNames } from '../../state/spreadsheet-metadata/selector'
+import { RootState         } from '../../state/store'
+
+import Button          from '../Button/Button'
+import Divider         from '../Divider/Divider'
+import Filter          from '../Filter/Filter'
+import { QueryAction } from '../Query/query-reducer'
+import Select          from '../Select/Select'
+
+import './QueryCreator.css'
+
+
+function QueryCreatorComponent(): JSX.Element {
+  const { sheetNames, filterConditions, submitQuery, state, dispatch } = useContext(QueryContext)
+  const { selectedSheetIndex } = state
+  const columnNames: string[] = useSelector((rootState: RootState) => selectColumnNames(rootState, selectedSheetIndex))
+  
+  if (!sheetNames) return <></>
+
+  return (
+    <div className='query-creator-container'>
+      <Select
+        title='Select a Sheet'
+        options={ sheetNames.map((name: string, index: number): SelectOption<number> => ({ label: name, value: index })) }
+        onChange={ (sheetIndex: number[]): void => dispatch({ type: QueryAction.SET_SHEET_INDEX, payload: sheetIndex[0] }) }
+      />
+      {
+        selectedSheetIndex !== -1 &&
+        <>
+          <Divider />
+          <Select
+            title='Include Columns'
+            options={ columnNames.map((name: string): SelectOption => ({ label: name })) }
+            onChange={ (columns: string[]): void => dispatch({ type: QueryAction.SET_INCLUDE_COLUMNS, payload: columns }) }
+            defaultSelections={ [columnNames.length] }
+            reset={ state.reset }
+            grid
+            multi
+          />
+          <Divider />
+          <Filter
+            onChange={ (conditions: QueryCondition[]): void => { filterConditions.current = conditions } }
+            reset={ state.reset }
+          />
+          <Divider />
+          <Button
+            name='submit-query'
+            onClick={ () => submitQuery(true) }
+            disabled={ state.queryInProgress }
+          >
+            Submit Query
+          </Button>
+        </>
+      }
+      { !!state.queryResponse?.results.length &&
+        <Button
+          name='clear-query'
+          onClick={ () => dispatch({ type: QueryAction.SET_QUERY_RESPONSE, payload: undefined }) }
+        >
+          Clear Query
+        </Button>
+      }
+    </div>
+  )
+}
+
+
+export default memo(QueryCreatorComponent)
