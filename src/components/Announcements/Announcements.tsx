d3f8231707a22f8741cf794f5e61d63c46bbbb44
Andrew Wanex <awanex2705@gmail.com>
2023-03-20 09:09:22 -0700

    feature: connect announcements component to server

--- src/components/Announcements/Announcements.tsx
+++ src/components/Announcements/Announcements.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+
+import { get          } from '../../http/client'
+import { Announcement } from '../../models/announcement'
 
 import './Announcements.css'
 
 
 function AnnouncementsComponent(): JSX.Element {
+  const [ message, setMessage ] = useState<string[]>([])
+  const [ page, setPage ] = useState<number>(0)
+  const announcements = useRef<Announcement[]>([])
+
+  useEffect(() => {
+    async function getAnnouncements() {
+      try {
+        const recentAnnouncements = await get<Announcement[]>('announcements')
+        if (recentAnnouncements) announcements.current = recentAnnouncements
+      } catch (error) {
+        console.log('announcements error', error)
+      }
+    }
+
+    getAnnouncements()
+  }, [])
+
+  
   return (
     <div className='announcements-container'>
-      TEST ANNOUNCEMENTS
+
     </div>
   )
 }
