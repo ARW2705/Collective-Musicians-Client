e01035723a86855cdded0c90823b3861cf320252
Andrew Wanex <awanex2705@gmail.com>
2023-03-20 12:35:03 -0700

    feature: add pagination to announcements

--- src/components/Announcements/Announcements.tsx
+++ src/components/Announcements/Announcements.tsx
@@ -1,21 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react'
 
-import { get          } from '../../http/client'
-import { Announcement } from '../../models/announcement'
+import { PaginationContext } from '../../contexts/pagination'
+import { get               } from '../../http/client'
+import { Announcement      } from '../../models/announcement'
+
+import Pagination from '../Pagination/Pagination'
 
 import './Announcements.css'
 
 
 function AnnouncementsComponent(): JSX.Element {
-  const [ message, setMessage ] = useState<string[]>([])
+  const [ announcement, setAnnouncement ] = useState<Announcement>()
   const [ page, setPage ] = useState<number>(0)
+  const [ pageCount, setPageCount ] = useState<number>(0)
   const announcements = useRef<Announcement[]>([])
 
   useEffect(() => {
     async function getAnnouncements() {
       try {
         const recentAnnouncements = await get<Announcement[]>('announcements')
-        if (recentAnnouncements) announcements.current = recentAnnouncements
+        if (recentAnnouncements && recentAnnouncements.length) {
+          announcements.current = recentAnnouncements
+            .sort((a1: Announcement, a2: Announcement): number => a2.importance - a1.importance)
+          setPageCount(announcements.current.length)
+          setAnnouncement(announcements.current[0])
+        }
       } catch (error) {
         console.log('announcements error', error)
       }
@@ -24,10 +33,16 @@ function AnnouncementsComponent(): JSX.Element {
     getAnnouncements()
   }, [])
 
+  useEffect(() => {
+    if (page < announcements.current.length) setAnnouncement(announcements.current[page])
+  }, [page])
   
   return (
-    <div className='announcements-container'>
-
+    <div className={ `announcements-container importance-${ announcement?.importance }` }>
+      <PaginationContext.Provider value={ { page, setPage, pageCount, pageLimit: 5 } }>
+        <p>{ announcement?.message }</p>
+        <Pagination type='pip' />
+      </PaginationContext.Provider>
     </div>
   )
 }
