2efe50dff2cfd12ace1fa84127abc25f99911b85
Andrew Wanex <awanex2705@gmail.com>
2023-04-03 12:25:35 -0700

    feature: update slice to allow multiple context sheets

--- src/state/spreadsheet-metadata/slice.ts
+++ src/state/spreadsheet-metadata/slice.ts
@@ -1,13 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+import { SheetColumnContext    } from '../../models/sheet-column-context'
 import { SpreadsheetMetadata } from '../../models/spreadsheet-metadata'
+import { SheetMetadata } from '../../models/sheet-metadata'
 
 
 const initialState: SpreadsheetMetadata = {
   id: null,
   name: null,
   sheets: [],
-  contextSheet: {}
+  sheetContext: {}
+}
+
+interface SheetContextPayload {
+  name: string
+  columnNames: string[]
+  context: { [columnName: string]: SheetColumnContext }
 }
 
 export const spreadsheetMetadataSlice = createSlice({
@@ -15,7 +23,18 @@ export const spreadsheetMetadataSlice = createSlice({
   initialState,
   reducers: {
     clear: (): SpreadsheetMetadata => initialState,
-    set: (_, action: { type: string, payload: SpreadsheetMetadata }): SpreadsheetMetadata => action.payload
+    set: (state: SpreadsheetMetadata, action: { type: string, payload: SpreadsheetMetadata | SheetContextPayload }): SpreadsheetMetadata => {
+      if (action.payload.hasOwnProperty('id')) return (action.payload as SpreadsheetMetadata)
+      if (state.id === null || action.payload === null) return state
+
+      const { name, columnNames, context } = action.payload as SheetContextPayload
+      const sheetIndex: number = state.sheets.findIndex((sheet: SheetMetadata) => sheet.name === name)
+      if (sheetIndex === -1) throw new Error(`Sheet with name ${name} not found`)
+
+      state.sheets[sheetIndex].columnNames = columnNames
+      state.sheetContext[name].columnContext = context
+      return state
+    }
   }
 })
 
