d55bebed3b717df3853eb6314f67159592703ff6
Andrew Wanex <awanex2705@gmail.com>
2023-04-03 12:56:12 -0700

    refactor: allow enum type selection if the selected column context allows doing so

--- src/components/FilterGroup/FilterGroup.tsx
+++ src/components/FilterGroup/FilterGroup.tsx
@@ -1,16 +1,21 @@
-import React, { memo, useContext, useRef, useState } from 'react'
+import React, { memo, useCallback, useContext, useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 
-import { QueryContext             } from '../../contexts/query'
-import { QueryCondition           } from '../../models/query-condition'
-import { QueryArgs                } from '../../models/query-args'
-import { SelectOption             } from '../../models/select-option'
-import { SingleQueryArgs          } from '../../models/single-query-args'
-import { ValidationError          } from '../../models/validation-error'
-import { FILTER_CONDITION_OPTIONS } from '../../shared/filter-condition-defs'
-import { required                 } from '../../shared/validation/validation'
-import { selectColumnNames        } from '../../state/spreadsheet-metadata/selector'
-import { RootState                } from '../../state/store'
+import { QueryContext        } from '../../contexts/query'
+import { QueryCondition      } from '../../models/query-condition'
+import { QueryArgs           } from '../../models/query-args'
+import { SelectOption        } from '../../models/select-option'
+import { SheetColumnContext  } from '../../models/sheet-column-context'
+import { SingleQueryArgs     } from '../../models/single-query-args'
+import { ValidationError     } from '../../models/validation-error'
+import { equalTo             } from '../../shared/constants/filter-condition-defs'
+import { getFilterConditions } from '../../shared/get-filter-conditions'
+import { required            } from '../../shared/validation/validation'
+import {
+  selectColumnNames,
+  selectColumnContext
+}                              from '../../state/spreadsheet-metadata/selector'
+import { RootState           } from '../../state/store'
 
 import Button        from '../Button/Button'
 import FilterPreview from '../FilterPreview/FilterPreview'
@@ -27,14 +32,20 @@ export interface FilterGroupProps {
 }
 
 function FilterGroupComponent({ onSubmit: handleOnSubmit, onRemove: handleOnRemove, groupKey }: FilterGroupProps): JSX.Element {
-  const { state } = useContext(QueryContext)
+  const { state, sheetNames } = useContext(QueryContext)
   const columnNames: string[] = useSelector((rootState: RootState) => selectColumnNames(rootState, state.selectedSheetIndex))
+  const columnContext: { [columnName: string]: SheetColumnContext } | undefined = useSelector((rootState: RootState) => (
+    state.selectedSheetIndex !== -1 ? selectColumnContext(rootState, sheetNames[state.selectedSheetIndex]) : undefined
+  ))
   const [ isDisabled, setIsDisabled ] = useState<boolean>(true)
   const [ filters, setFilters ] = useState<QueryCondition>()
   const [ reset, setReset ] = useState<boolean>(false)
+  const [ selectedColumn, setSelectedColumn ] = useState<{ column: string, errors: ValidationError<string> }>({ column: '', errors: {} })
+  const [ filterConditionOptions, setFilterConditionOptions ] = useState<SelectOption<string>[]>([])
+  const [ isEnum, setIsEnum ] = useState<boolean>(false)
   const filterPartial = useRef<{[key: string]: any}>({})
 
-  const onChange = (propName: string, value: string | number, errors: ValidationError<string | number>): void => {
+  const onChange = useCallback((propName: string, value: string | number, errors: ValidationError<string | number>): void => {
     if (Object.keys(errors).length > 0) {
       delete filterPartial.current[propName]
       setIsDisabled(true)
@@ -44,7 +55,7 @@ function FilterGroupComponent({ onSubmit: handleOnSubmit, onRemove: handleOnRemo
         setIsDisabled(false)
       }
     }
-  }
+  }, [isDisabled])
 
   const onSubmit = (): void => {
     if (!filterPartial.current) return
@@ -92,6 +103,27 @@ function FilterGroupComponent({ onSubmit: handleOnSubmit, onRemove: handleOnRemo
     setFilters(filtersUpdate)
   }
 
+  const handleEnumChange = (columns: string[], errors: ValidationError<string>) => {
+    onChange('condition', equalTo.value || '', {})
+    onChange('target', columns[0], errors)
+  }
+
+  const handleConditionChange = (columns: string[], errors: ValidationError<string>) => {
+    onChange('condition', columns[0], errors)
+  }
+
+  useEffect(() => {
+    onChange('column', selectedColumn.column, selectedColumn.errors)
+  }, [selectedColumn, onChange])
+
+  useEffect(() => {
+    if (!columnContext || !columnContext[selectedColumn.column]) return
+
+    const { dataType } = columnContext[selectedColumn.column]
+    setIsEnum(dataType.includes('enum'))
+    setFilterConditionOptions(getFilterConditions(dataType))
+  }, [selectedColumn, columnContext])
+
   return (
     <section className='filter-group-container'>
       <Button
@@ -100,37 +132,40 @@ function FilterGroupComponent({ onSubmit: handleOnSubmit, onRemove: handleOnRemo
         onClick={ () => handleOnRemove() }
         flat
       >
-        Remove Group
+        Remove Filter Group
       </Button>
       <div className='filter-controls'>
         <Select
           title='Column'
           customClass='filter-select'
           options={ columnNames.map((name: string): SelectOption => ({ label: name })) }
-          onChange={ (columns: string[], errors: ValidationError<string>) => onChange('column', columns[0], errors) }
+          onChange={ (columns: string[], errors: ValidationError<string>) => setSelectedColumn({ column: columns[0], errors }) }
           validators={ [required()] }
           reset={ reset }
-          grid
+          scroll
           optionAsTitle
         />
         <Select
-          title='Condition'
+          title={ isEnum ? 'Target' : 'Condition' }
           customClass='filter-select'
-          options={ FILTER_CONDITION_OPTIONS }
-          onChange={ (columns: string[], errors: ValidationError<string>) => onChange('condition', columns[0], errors) }
+          options={ filterConditionOptions }
+          onChange={ isEnum ? handleEnumChange : handleConditionChange }
           validators={ [required()] }
           reset={ reset }
-          grid
+          scroll
           optionAsTitle
         />
-        <Input
-          label='Target'
-          name='target'
-          type='text'
-          onChange={ onChange }
-          validators={ [required()] }
-          reset={ reset }
-        />
+        {
+          !isEnum &&
+          <Input
+            label='Target'
+            name='target'
+            type='text'
+            onChange={ onChange }
+            validators={ [required()] }
+            reset={ reset }
+          />
+        }
         <Button
           name='add-filter'
           customClass='add-filter-button'
